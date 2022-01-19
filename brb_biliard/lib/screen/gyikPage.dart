3b70d835c485dfd59e073516b90333b7d5374746
HernadiB <hernadi.barnabas@njit.hu>
2022-01-19 12:53:23 +0100

    F2.6 Flutter User GYIK oldal

--- brb_biliard/lib/screen/gyikPage.dart
+++ brb_biliard/lib/screen/gyikPage.dart
@@ -9,17 +9,44 @@ class gyikPage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Gyakran ismételt kérdések"),
           backgroundColor: const Color(0xffCE5D34),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.cancel),
-                onPressed: () {
-                  Navigator.of(context).push(new MaterialPageRoute(
-                    builder: (BuildContext context) => new LoginScreen(),
-                  ));
-                })
-          ],
         ),
         drawer: NavDrawer(),
-        body: Center(child: Text("Ez itt a GYIK oldal")));
+        body: Column(children: [
+          SizedBox(
+            height: 45,
+          ),
+          ListTile(
+            title: Text('Milyen bankkártyával fizethetek?'),
+            subtitle: Text(
+                'A vásárlásod a SimplePay fizető felületén keresztül zajlik. Az online vásárlások során elfogadunk minden olyan American Express, Maestro, MasterCard, Visa és Visa Electron típusú kártyát, melyek internetes fizetésre a kártyakibocsátó bank által engedélyeztetve lettek.'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 45,
+          ),
+          ListTile(
+            title: Text(
+                'Mi történik, ha a fizetési tranzakció során hiba lépett fel?'),
+            subtitle: Text(
+                'Az online vásárlás során csak azok a tranzakciók teljesülnek, amelyek során a fizetési folyamat minden lépése sikeresen lezajlott és amelynek a végén a SimplePay fizető felületéről visszajutottál a Sugár Mozi oldalára és ott megkaptad a visszaigazolást a tranzakcióról. Amennyiben a folyamatban hiba lépett fel, kérjük indítsd újra a böngésződet és kezdd újra a vásárlás folyamatát.'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 45,
+          ),
+          ListTile(
+            title: Text(
+                'Mi történik, ha véletlenül töröltem a visszaigazoló e-mailt?'),
+            subtitle: Text(
+                'A vásárlás során használt e-mail címedre lesz szükségünk. Ha nem kapsz időben választ, pénztárainknál e-mail címed bemondásával átveheted a jegyeket. '),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ]));
   }
 }
